924afcb02d67992e605fd9f12b37283915021131
Tim <tja2353@gmail.com>
2022-02-10 17:30:57 +0000

    updates

--- src/app/services/service-a.service.spec.ts
+++ src/app/services/service-a.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceAService } from './service-a.service';
+import { ServiceAService } from '@app/services/service-a.service';
+// import { ServiceAService } from './service-a.service';
 
 describe('ServiceAService', () => {
   let service: ServiceAService;
