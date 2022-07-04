905c4d09f03750f7a2a96993a1391aa1d8d7bb86
Tim <tja2353@gmail.com>
2022-07-04 15:27:00 +0100

    remove Cypress plugins folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 10000,
@@ -6,12 +6,17 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      // we can grab some process environment variables
+      // and stick it into config.env before returning the updated config
+      config.env = config.env || {};
+      // config.env.FIRESTORE_EMULATOR_HOST = process.env.FIRESTORE_EMULATOR_HOST;
+
+      return config;
     },
     baseUrl: 'http://localhost:4200',
   },
   retries: {
     runMode: 3,
     openMode: 2,
-  },  
-})
+  },
+});
