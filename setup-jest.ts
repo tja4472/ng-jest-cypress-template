547aa98595cb799dde486c1030032fd763f76310
Tim <tja2353@gmail.com>
2024-11-20 11:17:58 +0000

    Use setupZoneTestEnv()

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,7 +1,9 @@
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import '@testing-library/jest-dom';
 import { configure } from '@testing-library/angular';
 import { ReactiveFormsModule } from '@angular/forms';
+
+setupZoneTestEnv();
 
 configure({
   defaultImports: [ReactiveFormsModule],
